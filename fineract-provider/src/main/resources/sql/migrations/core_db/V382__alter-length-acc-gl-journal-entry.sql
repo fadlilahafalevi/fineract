a3e8886304e7d972dac727d3f9313fda9f67fe14
Fanny Agustiawan R <fanny.raharjo@anabatic.com>
2020-02-12 10:08:40 +0700

    20200212 | FannyAR | Fixing column name

--- fineract-provider/src/main/resources/sql/migrations/core_db/V382__alter-length-acc-gl-journal-entry.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V382__alter-length-acc-gl-journal-entry.sql
@@ -1,4 +1,4 @@
 alter table `acc_gl_journal_entry`
-	change `organization_running_balance` `overdraft_amount_derived` decimal(40, 6) null default null,
-	change `office_running_balance` `running_balance_derived` decimal(40, 6) null default null,
+	change `organization_running_balance` `organization_running_balance` decimal(40, 6) null default null,
+	change `office_running_balance` `office_running_balance` decimal(40, 6) null default null,
 	change `amount` `amount` decimal(40, 6) null default null;
